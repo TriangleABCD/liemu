d252346daa41c6aeb51accbc2866d641a9754f38
wangyaoyong <yaoyong.oerv@isrc.iscas.ac.cn>
2024-04-15 22:54:08 +0800

    add cmds

--- src/liemu.cpp
+++ src/liemu.cpp
@@ -8,16 +8,52 @@
 
 Machine m;
 
-// struct Command {
-//   std::string cmd_name;
-//   std::string descr;
-//   std::function<int(std::string)> handle_func;
-// };
-//
-// std::vector<Command> cmds {
-//   { "help", "show help info",  },
-//   { "q", "quit liemu" }, 
-// };
+struct Command {
+  std::string cmd_name;
+  std::string descr;
+  std::function<int(std::string)> handle_func;
+};
+
+int cmd_help(std::string cmd);
+int cmd_quit(std::string cmd);
+int cmd_continue(std::string cmd);
+int cmd_si(std::string cmd);
+int cmd_info(std::string cmd);
+
+std::vector<Command> cmds {
+  { "help", "show help info", cmd_help },
+  { "q", "quit liemu", cmd_quit },
+  { "c", "continue execute rest of insts", cmd_continue },
+  { "si", "go N steps", cmd_si },
+  { "info", "print value", cmd_info },
+};
+
+int cmd_help(std::string cmd) {
+  for (auto& c : cmds) {
+    printf("%s:\t%s\n", c.cmd_name.c_str(), c.descr.c_str());
+  }
+  return 0;
+}
+
+int cmd_quit(std::string cmd) {
+  printf("liemu quited.\n");
+  return 1;
+}
+
+int cmd_continue(std::string cmd) {
+  m.execute();
+  return 0;
+}
+
+int cmd_si(std::string cmd) {
+  m.execute_one_step();
+  return 0;
+}
+
+int cmd_info(std::string cmd) {
+  m.cpu.info_reg();
+  return 0;
+}
 
 int main(int argc, char* argv[]) {
   m.mem.write_vmem(START_ADDR, 0);
@@ -32,17 +68,17 @@ int main(int argc, char* argv[]) {
     input = readline("(liemu)");
     std::string cmd(input);
     free(input);
-    if ("q" == cmd || "quit" == cmd || "exit" == cmd) {
-      printf("liemu quited\n");
+    int r = -1;
+    for (auto& c : cmds) {
+      if (cmd == c.cmd_name) {
+        r = c.handle_func(cmd);
+        break;
+      }
+    }
+    if (-1 == r) {
+      fprintf(stderr, "wrong command\n");
+    } else if (1 == r) {
       break;
-    } else if ("si" == cmd) {
-      m.execute_one_step();
-    } else if ("c" == cmd) {
-      m.execute();
-    } else if ("info" == cmd) {
-      m.cpu.info_reg();
-    } else {
-      fprintf(stderr, "wrong command!\n");
     }
   }
 
