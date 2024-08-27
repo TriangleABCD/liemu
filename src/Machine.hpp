a4a8d1047a8d82bdfa1f44297d3a3234b01f0d88
Wang Yaoyong <wangyaoyong@njust.edu.cn>
2024-08-27 15:22:00 +0800

    sdb cmd done

--- src/Machine.hpp
+++ src/Machine.hpp
@@ -2,17 +2,17 @@
 #ifndef MACHINE_H
 #define MACHINE_H
 
+#include <map>
+
 #include "common.hpp"
 #include "CPU.hpp"
 #include "Memory.hpp"
 #include "Cache.hpp"
-#include "Watch.hpp"
 
 struct Machine {
   CPU cpu;
   Memory memory;
   Cache cache;
-  WatchList watchList;
 
   u32 readMem(u32 addr) {
     return self.memory.read(addr);
@@ -21,6 +21,7 @@ struct Machine {
   void writeMem(u32 addr, u32 data) {
     self.memory.write(addr, data);
   }
+
 };
 
 #endif
--- src/sdb.hpp
+++ src/sdb.hpp
@@ -11,12 +11,74 @@
 #include "common.hpp"
 #include "Machine.hpp"
 #include "Inst.hpp"
+#include "Watch.hpp"
 
 namespace Sdb {
 
-inline struct {
-  bool list = false;
-} config;
+inline std::map<u32, Inst> getParsedInst;
+
+inline WatchList watchList;
+
+inline void resetMachine(Machine& m) {
+  
+}
+
+
+inline void preParseInst(Machine& m) {
+  auto pc = m.cpu.pc;
+  while (1) {
+    u32 inst_value = m.readMem(pc);
+    if (inst_value == MAGIC) {
+      break;
+    }
+
+    auto inst =  parseInst(inst_value, m);
+    if (inst.result == -1) {
+      fprintf(stderr, RED("parse inst failed\n"));
+      break;
+    }
+
+    getParsedInst[pc] = inst;
+    pc += 4;
+  }
+}
+
+
+inline int execute_one_step(Machine& m) {
+  if (m.cpu.pc < CODE_START) {
+    return 1;
+  }
+
+  Inst inst = getParsedInst[m.cpu.pc];
+  if (inst.result == -1) {
+    fprintf(stderr, RED("finish\n"));
+    return -1;
+  }
+
+  int r = inst.doit(inst, m);
+  m.cpu.pc += 4;
+
+  if (-1 == r) {
+    fprintf(stderr, RED("exec error\n"));
+    return r;
+  }
+  return r != 0;
+}
+
+
+inline int execute(Machine& m) {
+  int r = 0;
+  while (1) {
+    r = execute_one_step(m);
+    if (r == -1 || r == 1) {
+      break;
+    }
+    if (watchList.update_watchpoint(m.cpu, m.memory)) {
+      break;
+    }
+  }
+  return r;
+}
 
 
 struct Command {
@@ -31,6 +93,7 @@ enum CmdResult {
   CMD_ERR = -1,
 };
 
+
 int cmd_help(Machine& m, const std::vector<std::string>& cmd);
 int cmd_quit(Machine& m, const std::vector<std::string>& cmd);
 int cmd_clear(Machine& m, const std::vector<std::string>& cmd);
@@ -41,6 +104,7 @@ int cmd_x(Machine& m, const std::vector<std::string>& cmd);
 int cmd_ls(Machine& m, const std::vector<std::string>& cmd);
 int cmd_w(Machine& m, const std::vector<std::string>& cmd);
 int cmd_d(Machine& m, const std::vector<std::string>& cmd);
+
 
 inline std::vector<Command> cmds {
   { "help", "show this help info", cmd_help },
@@ -55,6 +119,7 @@ inline std::vector<Command> cmds {
   { "d", "d watchpoint delete a watch point", cmd_d },
 };
 
+
 inline int cmd_help(Machine& m, const std::vector<std::string>& cmd) {
   for (auto& c : cmds) {
     printf(GREEN("%s")":\t%s\n", c.cmd_name.c_str(), c.cmd_descr.c_str());
@@ -67,20 +132,42 @@ inline int cmd_quit(Machine& m, const std::vector<std::string>& cmd) {
   return CmdResult::CMD_QUIT;
 }
 
+
 inline int cmd_clear(Machine& m, const std::vector<std::string>& cmd) {
   printf("\033[2J\033[H");
   return CmdResult::CMD_OK;
 }
 
-inline int cmd_continue(Machine& m, const std::vector<std::string>& cmd) {
 
-  return CmdResult::CMD_OK;
+inline int cmd_continue(Machine& m, const std::vector<std::string>& cmd) {
+  int r = execute(m);
+  return CmdResult(r);
 }
+
 
 inline int cmd_si(Machine& m, const std::vector<std::string>& cmd) {
+  int n = 1;
+  if (cmd.size() >= 2) {
+    try {
+      n = std::stoi(cmd[1]);
+    } catch (std::exception& e) {
+      return CmdResult::CMD_ERR;
+    }
+  }
 
-  return CmdResult::CMD_OK;
+  int r = 0;
+  for (int i = 0; i < n; i++) {
+    r = execute_one_step(m);
+    if (r == -1 || r == 1) {
+      break;
+    }
+    if (watchList.update_watchpoint(m.cpu, m.memory)) {
+      break;
+    }
+  }
+  return CmdResult(r);
 }
+
 
 inline int cmd_info(Machine& m, const std::vector<std::string>& cmd) {
   if (cmd.size() == 1) {
@@ -91,7 +178,7 @@ inline int cmd_info(Machine& m, const std::vector<std::string>& cmd) {
   }
   
   if (cmd[1] == "w") {
-    m.watchList.info_watchpoint(m.cpu, m.memory); 
+    watchList.info_watchpoint(m.cpu, m.memory); 
     return CmdResult::CMD_OK;
   }
 
@@ -108,23 +195,82 @@ inline int cmd_info(Machine& m, const std::vector<std::string>& cmd) {
   return CmdResult::CMD_OK;
 }
 
+
 inline int cmd_x(Machine& m, const std::vector<std::string>& cmd) {
+  if (cmd.size() <= 1 || cmd.size() > 3) {
+    return CmdResult::CMD_ERR;
+  }
+  
+  int n = atoi(cmd[1].c_str());
+  u32 addr = strtol(cmd[2].c_str()+2, nullptr, 16);
+
+  for (int i = 0; i < n; i++) {
+    u32 word = m.readMem(addr + i * 4);
+    unsigned int bytes[4] {
+      (word) & 0xff,
+      (word >> 8) & 0xff,
+      (word >> 16) & 0xff,
+      (word >> 24) & 0xff,
+    };
+    printf(GREEN("0x%08x")":\t0x%02x\t0x%02x\t0x%02x\t0x%02x\n", addr + i * 4, bytes[3], bytes[2], bytes[1], bytes[0]);
+  }
 
   return CmdResult::CMD_OK;
 }
+
 
 inline int cmd_ls(Machine& m, const std::vector<std::string>& cmd) {
-
+  u32 pc = m.cpu.pc;
+  if (m.readMem(pc) == MAGIC) {
+    return 0;
+  }
+  u32 beg = pc - 5 * sizeof(u32);
+  if (beg < 0x80000000) {
+    beg = 0x80000000;
+  }
+  printf(GREEN("##############################################################\n"));
+  for (int i = 0; ; i++) {
+    u32 cur = beg + i * sizeof(u32);
+    if (cur > pc && cur - pc > 5 * sizeof(u32)) {
+      break;
+    }
+    u32 inst = m.readMem(cur);
+    if (inst == MAGIC) {
+      break;
+    }
+    int line = (cur - 0x80000000) / sizeof(u32);
+    if (cur == pc) {
+      printf("\033[32m>%d\t", line+1);
+      printf("0x%08x\t", cur);
+      printf("%08x\t", inst);
+      printf("%s\033[0m\n", getParsedInst[cur].name.c_str());
+    } else {
+      printf(" %d\t", line+1);
+      printf("0x%08x\t", cur);
+      printf("%08x\t", inst);
+      printf("%s\n", getParsedInst[cur].name.c_str());
+    }
+  }
+  printf(GREEN("##############################################################\n"));
   return CmdResult::CMD_OK;
 }
+
 
 inline int cmd_w(Machine& m, const std::vector<std::string>& cmd) {
-
+  if (cmd.size() != 3) {
+    return CmdResult::CMD_ERR;
+  }
+  watchList.add_watchpoint(m.cpu, m.memory, cmd[1], cmd[2]);
   return CmdResult::CMD_OK;
 }
 
-inline int cmd_d(Machine& m, const std::vector<std::string>& cmd) {
 
+inline int cmd_d(Machine& m, const std::vector<std::string>& cmd) {
+  if (cmd.size() != 2) {
+    return CmdResult::CMD_ERR;
+  }
+  int id = atoi(cmd[1].c_str());
+  watchList.del_watchpoint(id);
   return CmdResult::CMD_OK;
 }
 
