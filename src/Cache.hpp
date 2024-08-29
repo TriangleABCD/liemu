910302b158e6d3568915cb13d854fd5437ec1229
Wang Yaoyong <wangyaoyong@njust.edu.cn>
2024-08-29 22:30:01 +0800

    rewrite cache struct

--- src/Cache.hpp
+++ src/Cache.hpp
@@ -8,61 +8,32 @@
 #include "common.hpp"
 #include "Memory.hpp"
 
-#define L1_CACHE_SIZE 8*KB
-#define L2_CACHE_SIZE 64*KB
-#define L3_CACHE_SIZE 512*KB
+#define L1_CACHE_BIT_SIZE 12
+#define L1_CACHE_SIZE     (1<<L1_CACHE_BIT_SIZE)
 
-#define BLOCK_SIZE    64
+#define L2_CACHE_BIT_SIZE 16
+#define L2_CACHE_SIZE     (1<<L2_CACHE_BIT_SIZE)
 
-struct CacheBlock {
-  bool valid;
-  bool dirty;
-  u32 tag;
-  u32 data[(BLOCK_SIZE>>2)];
+#define L3_CACHE_BIT_SIZE 20
+#define L3_CACHE_SIZE     (1<<L3_CACHE_BIT_SIZE)
 
-  void resetBlock() {
-    self.valid = false;
-    self.dirty = false;
-    self.tag = 0;
-    for (int i = 0; i < (BLOCK_SIZE>>2); i++) {
-      self.data[i] = MAGIC;
-    }
-  }
-};
+#define BLOCK_BIT_SIZE    6
+#define BLOCK_SIZE        (1<<BLOCK_BIT_SIZE)
 
-struct CacheSet {
-  int level;
+#define GROUP_BIT_SIZE    2
+#define GROUP_SIZE        (1<<GROUP_BIT_SIZE)
 
-  std::vector<CacheBlock> blocks;
+struct BasicCache {
+  std::vector<u32> data;
+  std::map<u32, bool> valid;
+  std::map<u32, bool> dirty;
+  std::map<u32, int> lfu_cnt;
+
   std::map<u32, bool> inCache;
-  std::map<u32, CacheBlock&> tag_map;
+  std::map<u32, int> addr2idx;
 
-  int total_cnt;
-  int hit_cnt;
-
-  CacheSet(int level = 3, int size = L1_CACHE_SIZE) {
-    self.level = level;
-    self.blocks.resize(size/BLOCK_SIZE);
-    for (auto& block : self.blocks) {
-      block.resetBlock();
-    }
-
-    self.total_cnt = 0;
-    self.hit_cnt = 0;
-  }
-
-  u32 read(Memory& mem, CacheSet& nextCache, u32 addr) {
-    assert(addr % 4 == 0);
-    if (self.inCache[addr]) {
-      self.total_cnt++;
-      self.hit_cnt++;
-      return self.tag_map[addr].data[addr%BLOCK_SIZE];
-    }
-  }
-
-  u32 write(Memory& mem, CacheSet& nextCache, u32 addr, u32 val) {
-
-  }
+  int cnt;
+  int hit;
 };
 
 struct ICache {
