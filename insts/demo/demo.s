b85a860eb50ff528daab7a896bb016cc99cfff08
Wang Yaoyong <yaoyong.oerv@isrc.iscas.ac.cn>
2024-10-08 17:03:30 +0800

    liemu done

--- insts/demo/demo.s
+++ insts/demo/demo.s
@@ -2,22 +2,42 @@
 
 _start:
   li a1, 1
-  beq a0, a1, TRAP
+  beq a0, a1, TRAP_1
   li a1, 2
-  beq a0, a1, TRAP
+  beq a0, a1, TRAP_2
   li a1, 3
-  beq a0, a1, TRAP
+  beq a0, a1, TRAP_3
   li a1, 4
-  beq a0, a1, TRAP
+  beq a0, a1, TRAP_4
 
-TRAP:
-  li a7, 0
+TRAP_1:
+  li a7, 1
   ecall
   add x0, x0, x0
   ecall
-  add x0, x0, x0
-  ecall
-  add x0, x0, x0
-  ecall
+  j RET
 
+TRAP_2:
+  li a7, 1
+  ecall
+  addi t0, t0, 1
+  addi t0, t0, -1
+  ecall
+  j RET
+
+TRAP_3:
+  li a7, 1
+  ecall
+  xori t1, t1, 0
+  ecall
+  j RET
+
+TRAP_4:
+  li a7, 1
+  ecall
+  andi t2, t2, 1
+  ecall
+  j RET
+
+RET:
   mret
